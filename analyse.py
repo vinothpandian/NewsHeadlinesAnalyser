253f34871f459e2585cd0f23f660107e60bae32a
Vinoth Pandian Sermuga Pandian <vinoth.pandian@rwth-aachen.de>
2017-01-21 12:27:47 +0100

    Added analysis code

--- analyse.py
+++ analyse.py
@@ -1,10 +1,11 @@
+import codecs
 import string
 import time
 
 import feedparser
-import codecs
 import tamil.utf8 as utf8
 from tabulate import tabulate
+
 tabulate.WIDE_CHARS_MODE = False
 
 
@@ -16,14 +17,18 @@ def parseXML(feedURL):
 
 def tamilAnalyse(titles):
     detailsTable = []
+    avgWordCount = 0
+    avgLetterCount = 0
     for title in titles:
         letters = utf8.get_letters(title)
-        words = utf8.get_tamil_words(letters)
+        words = utf8.get_words(letters)
         wordCount = len(words)
         letterCount = len(letters) - letters.count(' ')
         detailsTable.append([wordCount, letterCount])
+        avgWordCount += wordCount
+        avgLetterCount += letterCount
 
-    return detailsTable
+    return [detailsTable, avgWordCount / len(titles), avgLetterCount / len(titles)]
 
 
 def genericAnalyse(titles):
@@ -37,6 +42,7 @@ def genericAnalyse(titles):
 
 def analyse(language, rssFeed):
     titles = parseXML(rssFeed)
+    details = []
 
     if language == "tamil":
         details = tamilAnalyse(titles)
@@ -47,15 +53,27 @@ def analyse(language, rssFeed):
     else:
         print("Language not supported")
 
-    f = codecs.open('{}.txt'.format(time.strftime("%d-%m-%Y")), 'w', 'utf-8')
+    f = codecs.open('{}_dump.txt'.format(time.strftime("%d-%m-%Y")), 'w', 'utf-8')
     print("Language : {}, Source RSS Link : {}".format(language.title(), rssFeed), file=f)
-    print(tabulate(details, headers=["Word count", "Letter count"], tablefmt="grid", numalign="right"), file=f)
+    print("Headlines : ", file=f)
+    for title in titles:
+        print(title, file=f)
+    print(tabulate(details[0], headers=["Word count", "Letter count"], tablefmt="grid", numalign="right"), file=f)
+    f.close()
+
+    f = codecs.open('{}_Analysis.txt'.format(time.strftime("%d-%m-%Y")), 'w', 'utf-8')
+    print("\n\nLanguage : {}, Source RSS Link : {}".format(language.title(), rssFeed), file=f)
+    print('+' + '-' * 45 + '+', file=f)
+    print("|  Average Word count  | Average Letter count |", file=f)
+    print('+' + '-' * 45 + '+', file=f)
+    print("|{0:^22d}|{1:^22d}|".format(int(details[1]), int(details[2])), file=f)
+    print('+' + '-' * 45 + '+', file=f)
     f.close()
 
 
 def main():
     rssList = {}
-    with open("input.txt") as f:
+    with open("inputO.txt") as f:
         for line in f.readlines():
             text = line.split()
             rssList[text[0]] = text[1]
